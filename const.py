71b757f0c3a352450f7b27ad5f53eec8d156da2e
slava <slava2006tukin@mail.ru>
2024-10-16 00:37:51 +0300

    Merge remote-tracking branch 'origin/test22' into test22
    # Conflicts:
    #	users/models.py
    #	users/templates/edit_profile.html
    #	users/templates/reports.html
    #	users/templates/stat.html
    #	users/templates/user_list.html
    #	users/templates/verefi.html
    #	users/urls.py
    #	users/views.py

--- const.py
+++ const.py
@@ -1,5 +1,21 @@
+import os
+
+import django
 from telebot import TeleBot
 
-bot = TeleBot('7195501588:AAEVgfLdoRljnST4XpkMnNuDne0DGDmDilg')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DavinCHBot.settings')
+# django.setup()
+# from django.contrib.auth.models import Group
+
+bot = TeleBot('7071542790:AAGLBF9X6V-PDSqcJAPXkfaHBvFR8haowu0')
 
 simbols = '🤙👌🤘🤟✌️'
+
+# Group.objects.get_or_create(name='бан профиля')
+# Group.objects.get_or_create(name='изменение профиля')
+# Group.objects.get_or_create(name='создание аккаунтов')
+# Group.objects.get_or_create(name='статистика')
+# Group.objects.get_or_create(name='удаление профиля')
+# Group.objects.get_or_create(name='управление верефикацией')
+# Group.objects.get_or_create(name='управление жалобами')
+# Group.objects.get_or_create(name='управление рекламой')
